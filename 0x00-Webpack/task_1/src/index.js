9a876ebb25df1b4ff60706cd1cd3e00137261842
sheyidip <sheyidip@gmail.com>
2024-10-31 18:22:09 +0000

    task1

--- 0x00-Webpack/task_1/src/index.js
+++ 0x00-Webpack/task_1/src/index.js
@@ -1,18 +1,27 @@
 import $ from 'jquery';
-import { debounce } from 'lodash';
+import _ from 'lodash';
 
 let count = 0;
 
+// Function to update the counter
 function updateCounter() {
-    count += 1;
-
-    $('#count').text(`${count} clicks on the button`);
+    count += 1; // Increment the count
+    $('#count').text(`${count} clicks on the button`); // Update the paragraph
 }
 
+// Debounce the updateCounter function
+const debouncedUpdateCounter = _.debounce(updateCounter, 300);
 
-/// Binding the debounce function to the click function
-const debouncedUpdateCounter = debounce(updateCounter, 300);
-
+// Create elements and add them to the body
 $(document).ready(function() {
-    $('#myButton').on('click', debounceUpdateCounter);
+    const button = $('<button>Click me!</button>');
+    const countParagraph = $('<p id="count">0 clicks on the button</p>');
+
+    // Append elements to the body
+    $('body').append(countParagraph);
+    $('body').append(button);
+
+    // Bind the debounced function to the button click event
+    button.on('click', debouncedUpdateCounter);
 });
+
