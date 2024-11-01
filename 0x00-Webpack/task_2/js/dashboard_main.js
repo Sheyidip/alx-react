f357530178b1a6f409d4f6c9ec970f6cd89b8f87
sheyidip <sheyidip@gmail.com>
2024-11-01 12:06:59 +0000

    task 2

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -7,3 +7,10 @@ $(document).ready(function() {
     $('body').append('<p id="count"></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
 });
+
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} click on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
