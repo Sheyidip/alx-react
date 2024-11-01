8e1af6250099ac106def192a1c7f76e1acd12958
sheyidip <sheyidip@gmail.com>
2024-11-01 12:49:16 +0000

    webpack.config.js

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -15,12 +15,41 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // Additional options for other image formats
+              optipng: {
+                enabled: false, // Disable if you're using pngquant
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
       },
     ],
   },
   optimization: {
-    // Minimize output to avoid asset size warnings
-    minimize: true,
+    minimize: true, // Minimization to avoid asset size warnings
   },
 };
